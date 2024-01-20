83a7b017192fd32a8c02c988dea674ea2447894d
krishna100k <krishnaprasadawala@gmail.com>
2024-01-20 23:49:11 +0530

    changes

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from "express";
 import mongoose from "mongoose";
 import cors from "cors";
-import "dotenv/config";
+import dotenv from "dotenv"
 import userRoute from "./routes/user";
 import authRoute from "./routes/auth";
 import productRoute from "./routes/products";
@@ -12,14 +12,15 @@ import razorRoute from "./routes/razor"
 import path from "path";
 import bodyParser from "body-parser";
 
-
 const app: any = express();
 const port: number = 3000;
+
+dotenv.config();
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// MongoDB connection
+
 const connectToMongoose = async () => {
   try {
     const mongoURL = process.env.MONGODB_URL;
@@ -32,33 +33,36 @@ const connectToMongoose = async () => {
     console.log("MongoDB connected successfully");
   } catch (err) {
     console.log(err);
+    process.exit(1); 
   }
 };
 
+
+mongoose.connection.on("connected", () => {
+  console.log("MongoDB connection established");
+  
+
+  app.use(cors({ origin: '*' }));
+  
+  const uploadsPath = path.join(__dirname, '../uploads');
+  app.use('/uploads', express.static(uploadsPath));
+
+  app.use("/user", userRoute);
+  app.use("/auth", authRoute);
+  app.use("/product", productRoute);
+  app.use("/cart", cartRoute);
+  app.use("/orders", orderRoute);
+  app.use("/wish", wishlistRoute);
+  app.use("/razor", razorRoute);
+
+  app.get("/home", (req: Request, res: Response) => {
+    res.send("Hello, TypeScript with Express!");
+  });
+
+  app.listen(process.env.PORT || port, () => {
+    console.log(`Server is running on http://localhost:${process.env.PORT || port}`);
+  });
+});
+
+
 connectToMongoose();
-
-app.use(cors({
-  origin: '*'
-}));
-
-const uploadsPath = path.join(__dirname, '../uploads');
-app.use('/uploads', express.static(uploadsPath));
-
-
-
-app.use("/user", userRoute);
-app.use("/auth", authRoute);
-app.use("/product", productRoute);
-app.use("/cart", cartRoute);
-app.use("/orders", orderRoute);
-app.use("/wish", wishlistRoute );
-app.use("/razor", razorRoute );
-
-
-app.get("/home", (req: Request, res: Response) => {
-  res.send("Hello, TypeScript with Express!");
-});
-
-app.listen(process.env.PORT || port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
